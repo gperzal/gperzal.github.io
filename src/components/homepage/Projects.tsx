48f841f9749c43116d5e19e58fbd3b59ed37cdab
Guido Perez <gperzal@gmail.com>
2024-09-05 01:24:39 -0400

    :sparkles: correcciones projets

--- src/components/homepage/Projects.tsx
+++ src/components/homepage/Projects.tsx
@@ -86,39 +86,38 @@ const ProjectCard: React.FC<{ project: ProjectType }> = ({ project }) => {
           </div>
           <div>
             <span className="ml-4 lg:ml-8 mr-2 text-white">name:</span>
-            <span className="text-gray-400">'</span>
+            <span className="text-gray-400">{`'`}</span>
             <span className="text-amber-300">{project.name}</span>
-            <span className="text-gray-400">',</span>
+            <span className="text-gray-400">{`',`}</span>
           </div>
           <div className="ml-4 lg:ml-8 mr-2">
             <span className=" text-white">tools:</span>
-            <span className="text-gray-400">['</span>
+            <span className="text-gray-400">{`['`}</span>
             {project.tools.map((tool, i) => (
               <React.Fragment key={i}>
                 <span className="text-amber-300">{tool}</span>
                 {project.tools.length - 1 !== i && (
-                  <span className="text-gray-400">', '</span>
+                  <span className="text-gray-400">{`', '`}</span>
                 )}
               </React.Fragment>
             ))}
-            <span className="text-gray-400">'],</span>
+            <span className="text-gray-400">{`'],`}</span>
           </div>
           <div>
             <span className="ml-4 lg:ml-8 mr-2 text-white">myRole:</span>
-            <span className="text-orange-400"> '{project.role}'</span>
+            <span className="text-orange-400">{` '${project.role}'`}</span>
             <span className="text-gray-400">,</span>
           </div>
           <div className="ml-4 lg:ml-8 mr-2">
             <span className="text-white">Description:</span>
-            <span className="text-cyan-400"> '{project.description}'</span>
+            <span className="text-cyan-400">{` '${project.description}'`}</span>
             <span className="text-gray-400">,</span>
           </div>
           {project.tags && project.tags.length > 0 && (
             <div className="ml-4 lg:ml-8 mr-2">
               <span className="text-white">Tags:</span>
               <span className="text-cyan-400">
-                {" "}
-                ['{project.tags.join("', '")}']
+                {` ['${project.tags.join("', '")}']`}
               </span>
               <span className="text-gray-400">,</span>
             </div>
